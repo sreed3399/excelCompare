26ff04bcaa3fbdeeea6b9101a3bc968e43783622
sreed3399@gmail.com <150767560+sreed3399@users.noreply.github.com>
2024-02-27 13:52:34 -0600

    a

---
+++ test.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from pathlib import Path
+import base64
+
+# Initial page config
+
+st.set_page_config(
+     page_title='Streamlit cheat sheet',
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
